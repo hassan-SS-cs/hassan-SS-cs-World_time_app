476d023469cb77e94fe74774228022115850d21d
hassan-SS-cs <has597962@gmail.com>
2026-05-08 08:29:00 +0300

    add flag png , now thw whole app is working

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:world_time_app/services/world_time.dart';
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
 
@@ -28,6 +28,27 @@ class _ChooseLocationState extends State<ChooseLocation> {
   //   print('initstate');
   //   getData();
   // }
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+  void updateTime(index)async{
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    //navigate to home screen with the needed data 
+    Navigator.pop(context, {
+  'location': instance.location, 
+  'flag':  instance.flag,
+  'time': instance.time,
+  'isDaytime': instance.isDaytime,
+    });
+  }
   @override
   Widget build(BuildContext context) {
         print('build init state');
@@ -37,7 +58,26 @@ class _ChooseLocationState extends State<ChooseLocation> {
         backgroundColor: Colors.blue[900],
         title: Text('Choose a Location', style: TextStyle(color: Colors.white),),
       ),
-      body: Text('Choose Location Screen'),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1,horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                    updateTime(index) ;                 
+                    },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
+
+        ),
     );
   }
 }
